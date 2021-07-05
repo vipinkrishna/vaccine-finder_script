7f51d702c042cb6b0d4e81444a940cb1f8bbf7a4
vipinkrishna <vipinkrishna.in@gmail.com>
2021-07-05 13:40:04 +0530

    CUI Fine Tunings

--- vaccineFinder_v1.js
+++ vaccineFinder_v1.js
@@ -5,7 +5,7 @@
 (function () {
 
   // YOUR SETTINGS GOES HERE - PINCODES, AGE CATEGORY, DOSE
-  const pincodes = [671531]  //[671531]  //[671531, 671316]
+  const pincodes = [671531, 671316]  //[671531]  //[671531, 671316]
   let yourAgeCategory = 18  //18  //40  //45
   let yourDose = "FIRST"  //"FIRST"  //"SECOND"
 
@@ -41,12 +41,13 @@
   }
 
   (async function vaccineFinder() {
-    console.log(`[PASS: ${counter++}]`)
+    console.log(`%c[PASS: ${counter++}]`, 'color: red')
 
     session_counter = 1
 
     for (pincode of pincodes) {
       for (date of dates) {
+        console.log(`%c${pincode} - %c${date}`, 'color: yellow','color: orange')
         let endpoint = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=" + pincode + "&date=" + date
         let response = await fetch(endpoint)
         let { sessions } = await response.json()
@@ -54,24 +55,24 @@
         for (session of sessions) {
           const { date: datestamp, address, pincode, available_capacity, available_capacity_dose1, available_capacity_dose2, vaccine, min_age_limit } = session
           if (min_age_limit === yourAgeCategory && ((yourDose === "FIRST") ? available_capacity_dose1 : available_capacity_dose2) > 0) {
-            console.log('================================================================')
+            console.log('\n%c================================================================', 'color: yellow')
             console.log(`SESSION:${session_counter++} - ${yourDose} DOSE - AGE:${min_age_limit}`)
-            console.log('----------------------------------------------------------------')
-            console.log("Date: ", datestamp)
-            console.log("Vaccine Name: ", vaccine)
-            console.log("Address: ", address, " Pincode: ", pincode)
+            console.log('%c================================================================', 'color: yellow')
+            console.log("Date: %c" + datestamp, 'color: orange')
+            console.log("Vaccine Name: %c" + vaccine, 'color: cyan')
+            console.log("Address: " + address + " Pincode: %c" + pincode, 'color: yellow')
             console.log("Available Capacity: ", available_capacity)
             console.log("Available Capacity for DOSE 1: ", available_capacity_dose1)
             console.log("Available Capacity for DOSE 2: ", available_capacity_dose2)
-            console.log("================================================================\n\n\n\n\n")
+            console.log("%c================================================================\n\n", 'color: yellow')
             beep(2000)
-            await wait(2000)  //SESSION
           }  //IF
+          // await wait(2000)  //SESSION
         }  //FOR
       }  //FOR
     }  //FOR
     await wait(300000)  //PASS
-    pincodes.length && dates.length && yourAgeCategory && yourDose && vaccineFinder()  //RECURSIVE CALL
+    pincodes.length && dates.length && yourAgeCategory && yourDose && vaccineFinder()  //RECURSIVE
   })()
   return "https://github.com/vipinkrishna"
 })()
